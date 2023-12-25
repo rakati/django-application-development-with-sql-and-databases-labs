0a86b3fe65f80764280f25dca14c438d85226386
rakati <ouhaddounouredine@gmail.com>
2023-12-25 17:51:32 +0100

    Add CourseAdmin class for customization of Course admin page

--- lab-4/adminsite/admin.py
+++ lab-4/adminsite/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from .models import Course, Instructor, Lesson
 
 # Register your models here.
-admin.site.register(Course)
+
+
+class CourseAdmin(admin.ModelAdmin):
+    """The class is responsible for customization of field in admin page
+    for the Course model"""
+    fields = ['pub_date', 'name', 'description']
+
+
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Instructor)
