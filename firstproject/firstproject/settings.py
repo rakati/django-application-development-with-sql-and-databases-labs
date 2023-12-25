49e07487c50ad7a9f1bfd0b3214a64d169a805e8
rakati <ouhaddounouredine@gmail.com>
2023-12-25 14:02:30 +0100

    config database connection setting

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'firstproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lab1',
+        'USER': 'rakati',
+        'PASSWORD': 'r4015..',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
