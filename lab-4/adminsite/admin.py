51ec8843ab6b1eb0cc9aa4f84a103a4e4c60b929
rakati <ouhaddounouredine@gmail.com>
2023-12-25 18:53:50 +0100

    Inlining:associate lesson object into Course model managing page

--- lab-4/adminsite/admin.py
+++ lab-4/adminsite/admin.py
@@ -4,9 +4,16 @@ from .models import Course, Instructor, Lesson
 # Register your models here.
 
 
+class LessonInline(admin.StackedInline):
+    """associate lesson object into Course model managing page"""
+    model = Lesson
+    extra = 5
+
+
 class CourseAdmin(admin.ModelAdmin):
     """customize fields included in Course admin page"""
     fields = ['pub_date', 'name', 'description']
+    inlines = [LessonInline]
 
 
 class InstructorAdmin(admin.ModelAdmin):
