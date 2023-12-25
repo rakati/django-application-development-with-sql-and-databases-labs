6df802bced73996a9415a8b67e1b6b4228ab0f51
rakati <ouhaddounouredine@gmail.com>
2023-12-25 14:22:25 +0100

    add simple view using an index func

--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+'''First app views'''
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    """Home page for firstapp"""
+    # create a simple html page as a string
+    template = "<html>" \
+        "This is my first view" \
+        "</html>"
+    # return the template as content argument in HTTP response
+    return HttpResponse(content=template)
